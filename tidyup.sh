d2c5a42447ceec3120fdb11ec0d1b735a65a1ac0
Bira <bignacio@users.noreply.github.com>
2022-11-24 22:47:55 -0500

    chore: delete unused project files (#2)

--- tidyup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-clang-tidy --config-file=clang-tidy.config -p build $@
-
-#clang-tidy -checks='-*,readability-*,clang-analyzer-core.*,clang-analyzer-cplusplus.*,modernize-*,performance-*,concurrency-*,cppcoreguidelines-*,bugprone*' \
-#     --warnings-as-errors=* -header-filter=*.\(?!build\).* -p build $@
