50d3ccbeafe13f36a1884922d389acb5538bdef0
Bira Ignacio <cgbira@gmail.com>
2023-04-15 21:57:24 -0400

    only build in debug mode

--- ci-scripts/build_and_test_simplepool.sh
+++ ci-scripts/build_and_test_simplepool.sh
@@ -3,16 +3,14 @@
 set -e
 cd simple_pool
 
-for build_type in Release Debug ; do
-    rm -rf build
-    cmake -B build -DCMAKE_BUILD_TYPE=${build_type}
+rm -rf build
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
 
-    make -j2 -C build
-    build/dyn_mem_pool_test
-    build/dyn_mem_pool_fuzz
-    build/dyn_mem_pool_fuzz_addr
+make -j2 -C build
+build/dyn_mem_pool_test
+build/dyn_mem_pool_fuzz
+build/dyn_mem_pool_fuzz_addr
 
-    if [ "$CC" = "clang" ]; then
-        build/dyn_mem_pool_fuzz_mem
-    fi
-done
+if [ "$CC" = "clang" ]; then
+    build/dyn_mem_pool_fuzz_mem
+fi
